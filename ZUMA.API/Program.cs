9cad3a6235273f97659b49a4380d3b628760e6b7
tdohnal <tomas.dohnal46@seznam.cz>
2026-04-27 18:31:52 +0200

    poly

--- ZUMA.API/Program.cs
+++ ZUMA.API/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Polly;
+using Polly.Extensions.Http;
 using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
@@ -151,6 +153,27 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod();
     });
 });
+
+#endregion
+
+#region Polly
+
+// 1. Definujeme politiku pro Circuit Breaker (pokud se to 5x po sobě nepovede, na 30s stopneme provoz)
+var circuitBreakerPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+
+// 2. Definujeme Retry s exponenciálním zpětným rázem a Jitterem
+var retryPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .WaitAndRetryAsync(3, retryAttempt =>
+        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) // 2, 4, 8 sekund
+        + TimeSpan.FromMilliseconds(new Random().Next(0, 100))); // Jitter
+
+// 3. Registrace pojmenovaného klienta s "Policy Wrap"
+builder.Services.AddHttpClient("ZumaExternalClient")
+    .AddPolicyHandler(retryPolicy)
+    .AddPolicyHandler(circuitBreakerPolicy);
 
 #endregion
 
